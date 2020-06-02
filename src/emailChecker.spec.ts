b86818d6b98a8b5b6f1b9b3c27872009e7a5ea6f
Marcus Meier <marcus@mdmeier.com>
2020-06-02 02:32:51 -0500

    Add basic de-dupe

--- src/emailChecker.spec.ts
+++ src/emailChecker.spec.ts
@@ -5,11 +5,17 @@ describe('emailChecker', () => {
 
 	beforeEach(() => {
 		emailChecker = new EmailChecker();
-	})
+	});
 
 	it('should count the emails in a request', () => {
 		const testEmails = JSON.stringify(['one@gmail.com', 'two@gmail.com', 'three@gmail.com']);
 
 		expect(emailChecker.checkEmails(testEmails)).toEqual(3);
-	})
+	});
+
+	it('should not count duplicates', () => {
+		const testEmails = JSON.stringify(['one@gmail.com', 'one@gmail.com']);
+
+		expect(emailChecker.checkEmails(testEmails)).toEqual(1);
+	});
 });
--- src/emailChecker.ts
+++ src/emailChecker.ts
@@ -4,6 +4,11 @@ export class EmailChecker {
 
 	public checkEmails(request: any, requestParser: (request: any) => string[] = parseStringArray): number {
 		const emails = requestParser(request);
-		return emails.length;
+
+		const emailSet = new Set<string>();
+
+		emails.forEach(email => emailSet.add(email));
+
+		return emailSet.size;
 	}
 }
